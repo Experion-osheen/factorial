5bd82dc4aab639290e94c6826cfa5bb8010143dd
experion <osheen.varghese@experionglobal.com>
2016-10-27 13:00:58 +0530

    changed for  clearing bug

---
+++ fact.java
@@ -0,0 +1,13 @@
+import java.io.*;
+class fact
+{
+	public static void main()
+	{
+	system.out.print ("enter the number");
+	k=integer.parseInt(system.in.readline());
+	i;f=1;
+		for(i=1;i<=k;i++)
+		f=f*i;
+	system.out.print (f);
+	}
+}
